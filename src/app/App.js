e1caf19bb3961523e67e66a90132e376e7e6d1f1
snaztoz <hafidhmn@gmail.com>
2022-06-03 23:11:43 +0700

    Create basic app layout

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Grid, GridItem } from '@chakra-ui/react';
+
+import Video from './components/Video';
 
 function App() {
   return (
     <Box bg='teal' h='100vh' w='100vw'>
-      Hello World
+      <Grid h='100%' templateColumns='repeat(3, 1fr)' w='100%'>
+        <GridItem colSpan={2} bg='mistyrose'>
+          <Video />
+        </GridItem>
+        <GridItem colSpan={1} bg='black'></GridItem>
+      </Grid>
     </Box>
   );
 }
---
+++ src/app/components/Video.js
@@ -0,0 +1,12 @@
+import { Center, Spinner } from '@chakra-ui/react';
+
+function Video() {
+  return (
+    <Center h='100%' w='100%'>
+      <Spinner color='royalblue' size='xl'
+          speed='1s' thickness='4px' />
+    </Center>
+  );
+}
+
+export default Video;
