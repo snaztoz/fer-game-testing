5afc12c89e6763573cb0f7d98a43d0cc7ab7d1c2
snaztoz <hafidhmn@gmail.com>
2022-06-06 22:46:58 +0700

    Integrate webcam to app

--- src/app/components/Video.jsx
+++ src/app/components/Video.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Center, Spinner } from '@chakra-ui/react';
+import Webcam from 'react-webcam';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { Center, Icon } from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
 
 export default function Video() {
+  const isCamActive = useSelector((state) => state.recognizer.isCamActive);
+
   return (
     <Center bg="gray.900" h="100%" w="100%">
-      <Center bg="ghostwhite" borderRadius="lg" px="2em" py="1em">
-        <Spinner
-          color="teal"
-          size="xl"
-          speed="1s"
-          thickness="4px"
-        />
-      </Center>
+      {isCamActive
+        ? <Webcam width="70%" />
+        : (
+          <Icon
+            as={BsFillPersonFill}
+            color="teal"
+            h="25%"
+            w="25%"
+          />
+        )}
     </Center>
   );
 }
