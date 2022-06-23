582feb94785746fd72afb9ac7f71a5fb48cc6e6e
F-Pawamatra <feryadwar@gmail.com>
2022-06-23 23:18:40 +0700

    copas from video

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,36 +1,67 @@
-import React from 'react';
-import {
-  Box,
-  Flex,
-  Grid,
-  GridItem,
-} from '@chakra-ui/react';
-
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Video from './components/Video';
+/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/media-has-caption */
+/* eslint-disable no-undef */
+import React, { useState, useRef, useEffect } from 'react';
+import * as faceapi from 'face-api.js';
+import './App.css';
 
 export default function App() {
-  return (
-    <Box h="100vh" w="100vw">
-      <Grid h="100%" templateColumns="repeat(3, 1fr)" w="100%">
-        {/* Main Section */}
-        <GridItem colSpan={2}>
-          <Flex direction="column" h="100%">
-            <Box>
-              <Navbar />
-            </Box>
-            <Box flex="1">
-              <Video />
-            </Box>
-          </Flex>
-        </GridItem>
+  const videoHeight = 480;
+  const videoWidth = 640;
+  const [initializing, setInitializing] = useState(false);
+  const videoRef = useRef();
+  const canvasRef = useRef();
 
-        {/* Sidebar Section */}
-        <GridItem boxShadow="md">
-          <Sidebar />
-        </GridItem>
-      </Grid>
-    </Box>
+  const startVideo = () => {
+    navigator.getUserMedia({
+      video: {},
+    // eslint-disable-next-line no-return-assign
+    }, (stream) => videoRef.current.srcObject = stream);
+  };
+
+  const handleVideoOnPlay = () => {
+    setInterval(async () => {
+      if (initializing) {
+        setInitializing(false);
+      }
+      canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(videoRef.current);
+      const displaySize = {
+        width: videoWidth,
+        height: videoHeight,
+      };
+      faceapi.matchDimensions(canvasRef.current, displaySize);
+      // eslint-disable-next-line max-len
+      const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      canvasRef.current.getContext('2d').clearRect(0, 0, videoWidth, videoHeight);
+      faceapi.draw.drawDetections(canvasRef.current, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvasRef.current, resizedDetections);
+      console.log(detections);
+    }, 100);
+  };
+
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = `${process.env.PUBLIC_URL}/models`;
+      setInitializing(true);
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+      ]).then(startVideo);
+    };
+    loadModels();
+  }, []);
+
+  return (
+    <div className="App">
+      <span>{initializing ? 'initializing' : 'Ready'}</span>
+      <div className="display-flex justify-content-center">
+        <video ref={videoRef} autoPlay muted height={videoHeight} width={videoWidth} onPlay={handleVideoOnPlay} />
+        <canvas ref={canvasRef} className="position-absolute" />
+      </div>
+    </div>
   );
 }
